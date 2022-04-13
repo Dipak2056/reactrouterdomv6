8adb0a8ece44c4de86bf94c3b69e410bf9764d66
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-13 13:43:22 +1000

    added link in the nav component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -9,6 +9,11 @@ import Errorpage from "./pages/Errorpage";
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link> <br />
+        <Link to="/about">About</Link> <br />
+        <Link to="/contact">Contact</Link> <br />
+      </nav>
       <h1>hello world</h1>
       <Routes>
         <Route path="/" element={<Home />}></Route>
@@ -16,6 +21,7 @@ function App() {
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="*" element={<Errorpage />}></Route>
       </Routes>
+      <footer>this is the footer</footer>
     </Router>
   );
 }
