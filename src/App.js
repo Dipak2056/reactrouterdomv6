3ee1091fb30148cddc5488e7ab0d0a6f0cb62b09
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-13 16:00:42 +1000

    going2wrap here, i will build the movie app now

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Error from "./pages/Error";
-
+import News from "./components/News";
+import { useState } from "react";
+import axios from "axios";
 function App() {
+  const [news, setNews] = useState([]);
+  const fetchNews = () => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=2eb7eb7e15f447c087874580dc8cf671"
+      )
+      .then((response) => setNews(response.data.articles));
+    console.log(news);
+  };
+
   return (
     <div className="App">
       <h1>main page</h1>
-      <Router>
+      <Home />
+      <News news={news} fetchNews={fetchNews} key={news.id} />
+
+      {/* <Router>
         <Link to="/">Home</Link> <hr />
         <Link to="/about" path="/about">
           About
@@ -23,7 +38,7 @@ function App() {
           <Route path="/about" element={<About />}></Route>
           <Route path="*" element={<Error />}></Route>
         </Routes>
-      </Router>
+      </Router> */}
     </div>
   );
 }
