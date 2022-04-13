034d68c6e01f8b169eae4eaf0ef8ae7437ffc377
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-13 14:19:42 +1000

    time to add the page not found page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Error from "./pages/Error";
+import About from "./pages/About";
 
 function App() {
   return (
-    <Router>
-      <h1>Route Links heading</h1>
-      <Link to="/" path="/">
-        Home
-      </Link>
-      <Link to="/contact" path="/contact">
-        contact
-      </Link>
-      <Link to="/path" path="/about">
-        about
-      </Link>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/contact/:username" element={<Contact />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="*" element={<Error />}>
-          {" "}
-        </Route>
-      </Routes>
-      <footer>hey i build this app</footer>
-    </Router>
+    <div className="App">
+      <h1>main page</h1>
+      <Router>
+        <Link to="/">Home</Link> <hr />
+        <Link to="/about" path="/about">
+          About
+        </Link>{" "}
+        <span></span>
+        <Link to="/contact">Contact</Link>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,14 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 const Contact = () => {
   let navigate = useNavigate();
   let { username } = useParams();
-  return (
-    <div>
-      Contact for
-      <button onClick={() => navigate("/about")}>
-        change to about page {username}
-      </button>
-    </div>
-  );
+  return <div>contact</div>;
 };
 
 export default Contact;
